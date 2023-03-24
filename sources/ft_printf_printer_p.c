439b9089a6e78e75837b8b800a3089e469850dfe
blopez-f <brunolf73@gmail.com>
2023-03-24 22:26:40 +0100

    202303242226_update

--- sources/ft_printf_printer_p.c
+++ sources/ft_printf_printer_p.c
@@ -6,7 +6,7 @@
 /*   By: blopez-f <blopez-f@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/19 09:41:54 by blopez-f          #+#    #+#             */
-/*   Updated: 2023/03/24 21:41:12 by blopez-f         ###   ########.fr       */
+/*   Updated: 2023/03/24 22:21:49 by blopez-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,17 +21,13 @@ int	ft_printf_printer_p(unsigned long num)
 	numstr = ft_ptoa_lower(num);
 	if (numstr == 0)
 		return (-1);
-	if (!numstr)
-	{
-		flag_result = write(1, "0x0", 3);
-		if (flag_result == -1)
-			return (-1);
-		return (4);
-	}
 	numlen = ft_strlen(numstr);
 	flag_result = write (1, numstr, numlen);
 	free(numstr);
+	return (flag_result);
+	/*
 	if (flag_result == -1)
 		return (-1);
 	return (numlen);
+	*/
 }
