4c99a4b20c0a222279429b932c1702f463610416
blopez-f <brunolf73@gmail.com>
2023-04-02 22:09:56 +0200

    202304022209_update

--- sources/ft_printf_printer_c.c
+++ sources/ft_printf_printer_c.c
@@ -6,15 +6,15 @@
 /*   By: blopez-f <blopez-f@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 17:15:59 by blopez-f          #+#    #+#             */
-/*   Updated: 2023/03/30 18:43:22 by blopez-f         ###   ########.fr       */
+/*   Updated: 2023/04/02 21:58:30 by blopez-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 
-int ft_printf_printer_c(int c)
+int	ft_printf_printer_c(int c)
 {
-	int flag_result;
+	int	flag_result;
 
 	flag_result = write(1, &c, 1);
 	if (flag_result == -1)
--- sources/ft_printf_printer_p.c
+++ sources/ft_printf_printer_p.c
@@ -6,31 +6,11 @@
 /*   By: blopez-f <blopez-f@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/19 09:41:54 by blopez-f          #+#    #+#             */
-/*   Updated: 2023/03/30 17:52:24 by blopez-f         ###   ########.fr       */
+/*   Updated: 2023/04/02 22:00:06 by blopez-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
-
-
-/*
-int	ft_printf_printer_p(unsigned long num)
-{
-	char	*numstr;
-	int		numlen;
-
-	numstr = ft_ptoa_lower(num);
-	if (!numstr)
-	{
-		ft_putstr_fd("0x0", 1);
-		return (3);
-	}
-	numlen = ft_strlen(numstr);
-	ft_putstr_fd(numstr, 1);
-	free(numstr);
-	return (numlen);
-}
-*/
 
 int	ft_printf_printer_p(unsigned long num)
 {
@@ -45,31 +25,4 @@ int	ft_printf_printer_p(unsigned long num)
 	flag_result = write (1, numstr, numlen);
 	free(numstr);
 	return (flag_result - 1);
-
 }
-
-/*
-int	ft_printf_printer_p(unsigned long num)
-{
-	char	*numstr;
-	int		numlen;
-	int		flag_result;
-
-	numstr = ft_ptoa_lower(num);
-	if (!numstr)
-	{
-		flag_result = write(1, "0x0", 3);
-		if (flag_result == -1)
-			return (-1);
-		return (3);
-	}
-	numlen = ft_strlen(numstr);
-	write(1, numstr, ft_strlen(numstr));
-	flag_result = write(1, numstr, ft_strlen(numstr));
-	free(numstr);
-	if (flag_result == -1)
-		return (-1);
-	return (numlen);
-}
-*/
-
